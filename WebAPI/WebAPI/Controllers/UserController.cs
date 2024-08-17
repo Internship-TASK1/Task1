7b3128cb38987330a0b76ad47f297b0a7494065d
Quynh-Tay-Ninh-70 <164440430+Quynh-Tay-Ninh-70@users.noreply.github.com>
2024-08-18 02:30:26 +0700

    Role

---
+++ WebAPI/WebAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using DAL.Entities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebAPI.Controllers
+{
+    [Authorize(Roles = "User")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("You have accessed the User controller.");
+        }
+    }
+}
