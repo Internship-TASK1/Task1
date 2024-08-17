935a47fd9623cd8f88ea8502be814a5b7217fbb7
RAKAN <143574840+47puyr@users.noreply.github.com>
2024-08-18 02:19:13 +0700

    Login

---
+++ WebAPI/DAL/Entities/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Entities
+{
+    public class Login
+    {
+         public string Username { get; set; } = string.Empty;
+         public string Password { get; set; } = string.Empty;
+
+    }
+}
---
+++ WebAPI/DAL/Entities/Register.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Register
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
