97ad7c050443082c5ad399618520e9a3d834563d
dquangcuong <127744213+dquangcuong@users.noreply.github.com>
2024-08-17 15:03:34 +0700

    CRUD Category

---
+++ WebAPI/DAL/Repositories/CategoryRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DbContext _context;
+
+        public CategoryRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Set<Category>().ToListAsync();
+        }
+
+        public async Task<Category?> GetCategoryByIdAsync(Guid id)
+        {
+            return await _context.Set<Category>().FindAsync(id);
+        }
+
+        public async Task AddCategoryAsync(Category category)
+        {
+            await _context.Set<Category>().AddAsync(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            _context.Set<Category>().Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCategoryAsync(Guid id)
+        {
+            var category = await _context.Set<Category>().FindAsync(id);
+            if (category != null)
+            {
+                _context.Set<Category>().Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
