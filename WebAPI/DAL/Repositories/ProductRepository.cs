1e11d07c0877b97422c6f26d1619b3341d0bb642
dquangcuong <127744213+dquangcuong@users.noreply.github.com>
2024-08-18 09:56:13 +0700

    merge to main

---
+++ WebAPI/DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MyDbContext _context;
+
+        public ProductRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(Guid id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync(); // Ensure MyDbContext inherits from DbContext
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync(); // Ensure MyDbContext inherits from DbContext
+        }
+
+        public async Task DeleteAsync(Product product)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync(); // Ensure MyDbContext inherits from DbContext
+        }
+    }
+}
