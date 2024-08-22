93989b76506015712baea1e1a6f2749381f09fb3
PhamMinhDuy1306 <164139833+PhamMinhDuy1306@users.noreply.github.com>
2024-08-22 13:42:31 +0700

    Add-Task

---
+++ Task1-main/WebAPI/DAL/Entities/Tasks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    [Table("Tasks")]
+    public class Tasks
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid OrderId { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedTime { get; set; }
+
+        [Required]
+        public Guid AssignedToUserId { get; set; }
+
+        [Required]
+        public Guid CreatedByUserId { get; set; }
+
+        public string? Description { get; set; }
+
+        [ForeignKey("OrderId")]
+        public Order? Order { get; set; }
+
+        [ForeignKey("AssignedToUserId")]
+        public User? AssignedToUser { get; set; }
+
+        [ForeignKey("CreatedByUserId")]
+        public User? CreatedByUser { get; set; }
+    }
+}
