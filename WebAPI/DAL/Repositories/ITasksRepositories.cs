e5a9f09d5ba8f574b0bf020a0b581862edbcbe10
PhamMinhDuy1306 <164139833+PhamMinhDuy1306@users.noreply.github.com>
2024-08-22 10:12:34 +0700

    add Task

---
+++ WebAPI/DAL/Repositories/ITasksRepositories.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAL.Repositories
+{
+    public interface ITasksRepositories
+    {
+        Task<IEnumerable<Tasks>> GetAllAsync();
+        Task<Tasks?> GetByIdAsync(Guid id);
+        Task AddAsync(Tasks tasks);
+        Task UpdateAsync(Tasks tasks);
+        Task DeleteAsync(Tasks tasks);
+    }
+}
