c45787377226b84da9c62de47ed9ea13a36b5c7f
PhamMinhDuy1306 <164139833+PhamMinhDuy1306@users.noreply.github.com>
2024-08-22 10:14:22 +0700

    Update MyDbContext.cs

--- WebAPI/DAL/MyDbContext.cs
+++ WebAPI/DAL/MyDbContext.cs
@@ -7,6 +7,7 @@ namespace DAL
 {
     public class MyDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Tasks> Tasks { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
@@ -43,6 +44,24 @@ namespace DAL
                 .WithMany(u => u.ProcessedOrders)
                 .HasForeignKey(o => o.ProcessedByUserId)
                 .OnDelete(DeleteBehavior.SetNull); // Adjust if needed
+
+            modelBuilder.Entity<Tasks>()
+                .HasOne(t => t.Order)
+                .WithMany(o => o.Tasks)
+                .HasForeignKey(t => t.OrderId)
+                .OnDelete(DeleteBehavior.Cascade);
+            
+            modelBuilder.Entity<Tasks>()
+                .HasOne(t => t.AssignedToUser)
+                .WithMany(u => u.AssignedTasks)
+                .HasForeignKey(t => t.AssignedToUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            modelBuilder.Entity<Tasks>()
+                .HasOne(t => t.CreatedByUser)
+                .WithMany(u => u.CreatedTasks)
+                .HasForeignKey(t => t.CreatedByUserId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
