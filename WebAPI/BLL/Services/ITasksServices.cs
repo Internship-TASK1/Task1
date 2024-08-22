a9de3a7accbab68cf909c81b980d5c2b097c0449
PhamMinhDuy1306 <164139833+PhamMinhDuy1306@users.noreply.github.com>
2024-08-22 10:15:26 +0700

    add Task

---
+++ WebAPI/BLL/Services/ITasksServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+using DAL.Repositories;
+
+namespace BLL.Services
+{
+    public interface ITasksServices
+    {
+        Task<IEnumerable<Tasks>> GetAllTasksAsync();
+        Task<Tasks?> GetTasksByIdAsync(Guid id);
+        Task AddTasksAsync(Tasks tasks);
+        Task UpdateTasksAsync(Tasks tasks);
+        Task DeleteTasksAsync(Guid id);
+    }
+}
